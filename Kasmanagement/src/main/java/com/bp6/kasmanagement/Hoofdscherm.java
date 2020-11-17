4a1783cc890d3c91704c4f708c97713b583438db
dahir123 <64265991+dahir123@users.noreply.github.com>
2020-11-17 14:19:29 +0100

    test
    test

--- Kasmanagement/src/main/java/com/bp6/kasmanagement/Hoofdscherm.java
+++ Kasmanagement/src/main/java/com/bp6/kasmanagement/Hoofdscherm.java
@@ -5,31 +5,44 @@
  */
 package com.bp6.kasmanagement;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.control.ToolBar;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 /**
  *
  * @author dahir
  */
-public class Hoofdscherm extends GridPane {
+public class Hoofdscherm extends BorderPane {
     
-    ToolBar toolBar = new ToolBar();
+   private HBox menu = new HBox();
+   
     
     public Hoofdscherm(){
         
         Button button1 = new Button("Button 1");
-        toolBar.getItems().add(button1);
-
-        Button button2 = new Button("Button 2");
-        toolBar.getItems().add(button2);
-
-        VBox vBox = new VBox(toolBar);
+        Button button2 = new Button("Button 2");        
+        Button button3 = new Button("Button 3");
+        
+        button1.setPrefSize(100, 20);
+        button2.setPrefSize(100, 20);
+        button3.setPrefSize(100, 20);
+        
+        menu.setPadding(new Insets(15, 12, 15, 12));
+        menu.setSpacing(10);
+        menu.getChildren().addAll(button1, button2, button3);
         
         
-        this.getChildren().addAll(vBox);
+        
+        this.setTop(menu);
+      
+        
+        
+        
+        
+        
+        
         
       
     }
