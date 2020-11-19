e97ba73c49e3f03f0d12b9a91e9b97680501d580
dahir123 <64265991+dahir123@users.noreply.github.com>
2020-11-19 19:07:33 +0100

    Inlogscherm
    inlogscherm doorontwikkeld

--- Kasmanagement/src/main/java/com/bp6/kasmanagement/view/Beheerderscherm.java
+++ Kasmanagement/src/main/java/com/bp6/kasmanagement/view/Beheerderscherm.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.bp6.kasmanagement;
+package com.bp6.kasmanagement.view;
 
 import java.sql.Connection;
 import java.sql.SQLException;
