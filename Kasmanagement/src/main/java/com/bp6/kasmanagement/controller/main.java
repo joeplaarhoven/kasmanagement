9f9bf37395aa513d298f2c18fa47492a9149724b
SavingPrivateHenk <61140909+SavingPrivateHenk@users.noreply.github.com>
2020-11-26 17:00:08 +0100

    Update main.java

--- Kasmanagement/src/main/java/com/bp6/kasmanagement/controller/main.java
+++ Kasmanagement/src/main/java/com/bp6/kasmanagement/controller/main.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.bp6.kasmanagement.controller;
 
 import com.bp6.kasmanagement.view.Rootpane;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
