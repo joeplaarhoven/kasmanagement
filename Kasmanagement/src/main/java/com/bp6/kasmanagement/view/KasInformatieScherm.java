95ba5c5c0bd3b3b2467843bf387b2dcdc8996d3b
Joep Laarhoven <laarhoven.joep@gmail.com>
2020-12-01 12:18:58 +0100

    kas update, attach kas to user

--- Kasmanagement/src/main/java/com/bp6/kasmanagement/view/KasInformatieScherm.java
+++ Kasmanagement/src/main/java/com/bp6/kasmanagement/view/KasInformatieScherm.java
@@ -13,14 +13,19 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -36,7 +41,8 @@ public class KasInformatieScherm extends BorderPane {
     private GridPane main_gridpane;
     private HBox hbox_labels, hbox_textfields;
     Label lbl_kasName, lbl_product;
-    TextField txt_kasName, txt_product;
+    TextField txt_kasName;
+    ComboBox cb_product;
     Button btn_addKas;
     TableView table;
 
@@ -49,6 +55,7 @@ public class KasInformatieScherm extends BorderPane {
         
         table = new TableView();
         ObservableList<KasInformatie> items = FXCollections.observableArrayList();
+        ObservableList<String> itemsProduct = FXCollections.observableArrayList();
         
         Connection con1 = null;
             try {
@@ -61,6 +68,8 @@ public class KasInformatieScherm extends BorderPane {
                         "LEFT JOIN gebruikerKas\n" +
                         "ON kas.kasNummer = gebruikerKas.kasNummer\n" +
                         "WHERE gebruikersNaam = '" + lblUser.getText() + "'");
+                
+                 
                 while(result1.next()){
                     String strKasNaam= result1.getString("kasNaam");
                     Integer strKasNummer = result1.getInt("kasNummer");
@@ -70,6 +79,15 @@ public class KasInformatieScherm extends BorderPane {
                     KasInformatie kas1 = new KasInformatie(strKasNummer, strKasNaam, strproduct, strDatum);
                     items.add(kas1);
                 }
+                
+                ResultSet resultProduct = stat.executeQuery("SELECT * FROM idealegroei");
+                  while(resultProduct.next()){
+                    String strProductNaam= resultProduct.getString("product");
+
+                   
+                    itemsProduct.add(strProductNaam);
+                }
+                 
 
 
 
@@ -83,11 +101,11 @@ public class KasInformatieScherm extends BorderPane {
                 }
             }
             
-        System.out.print(items);
+           
             
         TableColumn kasNaamCol = new TableColumn("Kas naam");
         kasNaamCol.setCellValueFactory(new PropertyValueFactory<KasInformatie,String>("kasNaam"));
-        TableColumn productCol = new TableColumn("Product");
+        TableColumn productCol = new TableColumn("product");
         productCol.setCellValueFactory(new PropertyValueFactory<KasInformatie,String>("product"));
         TableColumn datumCol = new TableColumn("Datum");
         datumCol.setCellValueFactory(new PropertyValueFactory<KasInformatie,String>("datumTijd"));
@@ -108,7 +126,10 @@ public class KasInformatieScherm extends BorderPane {
 
         // Sets textfields
         txt_kasName = new TextField();
-        txt_product = new TextField();
+        
+        //sets combobox
+        cb_product = new ComboBox(itemsProduct);
+        cb_product.setPromptText("Select Product");
         
         // Sets button
         btn_addKas = new Button("Kas Toevoegen");
@@ -123,7 +144,7 @@ public class KasInformatieScherm extends BorderPane {
 
         // Sets width textfields
         txt_kasName.setPrefWidth(150);
-        txt_product.setPrefWidth(150);
+        cb_product.setPrefWidth(150);
         
         // Sets width button
         btn_addKas.setPrefWidth(150);
@@ -140,7 +161,7 @@ public class KasInformatieScherm extends BorderPane {
         
         // Places the labels in the Hbox
         hbox_labels.getChildren().addAll(lbl_kasName, lbl_product);
-        hbox_textfields.getChildren().addAll(txt_kasName, txt_product, btn_addKas);
+        hbox_textfields.getChildren().addAll(txt_kasName, cb_product, btn_addKas);
 
         // Position in gridpane
         main_gridpane.add(hbox_labels, 0, 1);
@@ -148,10 +169,75 @@ public class KasInformatieScherm extends BorderPane {
         main_gridpane.add(table, 0, 3);
         this.setLeft(main_gridpane);
         
+        table.setOnMouseClicked((MouseEvent event) -> {
+            if(event.getButton().equals(MouseButton.PRIMARY)){
+                txt_kasName.setText(table.getSelectionModel().getSelectedItem().toString());
+                btn_addKas.setOnAction(e -> {
+            
+                    System.out.print(cb_product.getValue());
+                    table.getItems().clear();
+                    java.util.Date dt = new java.util.Date();
+
+                    java.text.SimpleDateFormat sdf =
+                         new java.text.SimpleDateFormat("yyyy-MM-dd");
+
+                    String currentTime = sdf.format(dt);
+                    Connection con2 = null;
+                    try {
+
+                        con2 = DBCPDataSource.getConnection();
+                        Statement stat = con2.createStatement();
+                        if(cb_product.getValue().equals(null)){
+                            boolean result = stat.execute("UPDATE kas\n" +
+                                "SET kasNaam = '" + txt_kasName.getText() + "'\n" +
+                                "WHERE kasNaam = '" + txt_kasName.getText() + "'");
+                            
+
+                        }
+                        else {
+                            boolean result = stat.execute("UPDATE kas\n" +
+                                "SET product = '"+cb_product.getValue()+"', datum = '"+currentTime+"', kasNaam = '" + txt_kasName.getText() + "'\n" +
+                                "WHERE kasNaam = '" + txt_kasName.getText() + "'");
+                            
+
+                        }
+
+
+                        ResultSet result1 = stat.executeQuery("SELECT *\n" +
+                                "FROM kas\n" +
+                                "LEFT JOIN gebruikerKas\n" +
+                                "ON kas.kasNummer = gebruikerKas.kasNummer\n" +
+                                "WHERE gebruikersNaam = '" + lblUser.getText() + "'");
+                        while(result1.next()){
+                            String strKasNaam= result1.getString("kasNaam");
+                            Integer strKasNummer = result1.getInt("kasNummer");
+                            String strDatum = result1.getString("datum");
+                            String strproduct = result1.getString("product");
+
+                            KasInformatie kas1 = new KasInformatie(strKasNummer, strKasNaam, strproduct, strDatum);
+
+                            items.add(kas1);
+                        }
+                        table.refresh();
+
+
+
+                    } catch (SQLException se) {
+                        se.printStackTrace();
+                    } finally {
+                        try {
+                            con2.close();
+                        } catch (SQLException SQLe) {
+
+                        }
+                    }
+                });
+            }
+        });
+
         btn_addKas.setOnAction(e -> {
-            System.out.print("test");
-            System.out.print(lblUser.getText());
-            System.out.print("test");
+            
+            System.out.print(cb_product.getValue());
             table.getItems().clear();
             java.util.Date dt = new java.util.Date();
 
@@ -164,12 +250,12 @@ public class KasInformatieScherm extends BorderPane {
 
                 con2 = DBCPDataSource.getConnection();
                 Statement stat = con2.createStatement();
-                if(txt_product.getText().equals("")){
+                if(cb_product.getValue().equals(null)){
                     boolean result = stat.execute("INSERT INTO kas (kasNaam, datum) VALUES('" + txt_kasName.getText()+ "', '"+currentTime+"')");
                     ResultSet resultKasNummer = stat.executeQuery("SELECT kasNummer FROM kas WHERE kasNaam = '"+ txt_kasName.getText() +"'");
                     Integer kasNummer;
                     if(resultKasNummer.next())
-        {
+                    {
                         kasNummer= resultKasNummer.getInt("kasNummer");
                         
                         boolean result1 = stat.execute("INSERT INTO gebruikerkas VALUES(" + kasNummer + ", '"+ lblUser.getText() +"')");
@@ -177,7 +263,16 @@ public class KasInformatieScherm extends BorderPane {
                     
                 }
                 else {
-                    boolean result = stat.execute("INSERT INTO kas (kasNaam, product, datum) VALUES('" + txt_kasName.getText()+ "', '"+ txt_product.getText()+ "', '"+currentTime+"')");
+                    boolean result = stat.execute("INSERT INTO kas (kasNaam, product, datum) VALUES('" + txt_kasName.getText()+ "', '"+ cb_product.getValue()+ "', '"+currentTime+"')");
+                    ResultSet resultKasNummer = stat.executeQuery("SELECT kasNummer FROM kas WHERE kasNaam = '"+ txt_kasName.getText() +"'");
+                    Integer kasNummer;
+                    if(resultKasNummer.next())
+                    {
+                        kasNummer= resultKasNummer.getInt("kasNummer");
+                        
+                        boolean result1 = stat.execute("INSERT INTO gebruikerkas VALUES(" + kasNummer + ", '"+ lblUser.getText() +"')");
+                    }
+                    
                 }
                 
 
